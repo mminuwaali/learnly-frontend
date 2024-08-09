c494b0b8fa3340bca36afb1ec55a728ba7dc2095
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-08-09 16:08:06 +0100

    created the hero component

---
+++ src/components/HeroView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="w-full h-[clamp(30em,calc(100vw/2),50em)] gap-4 bg-gray-900 relative flex items-center justify-center md:justify-start p-[8%]">
+    <div class="flex flex-col p-4 gap text-white text-center md:text-left">
+        <span style="font-variant: small-caps;" class="text-xl md:text-3xl">homemade category</span>
+        <h2 style="font-variant: small-caps;" class="text-3xl md:text-7xl font-bold">lucky shop</h2>
+    </div>
+  </section>
+</template>
+
+<style></style>
