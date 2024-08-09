53e5fb79aca57f9d489037432b4c36fd6b03bf60
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-08-09 16:06:30 +0100

    configured the api request instance

---
+++ src/utilities/request.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({ baseURL: "http://localhost:3000/api" });
+
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+}, Promise.reject);
+
+export default instance;
