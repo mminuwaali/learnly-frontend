5f09b6110bc8bbf995ceb6abf439012a560157d5
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-08-09 16:05:58 +0100

    created the main layout

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HeaderView from "../HeaderView.vue";
+import FooterView from "../FooterView.vue";
+</script>
+
+<template>
+  <HeaderView />
+
+  <main class="w-full grow flex flex-col">
+    <RouterView />
+  </main>
+
+  <FooterView />
+</template>
+
+<style></style>
